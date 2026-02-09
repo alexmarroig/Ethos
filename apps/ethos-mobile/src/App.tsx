c581fa825addeb5513f9370c088d62911e475664
alex <82953425+alexmarroig@users.noreply.github.com>
2026-02-09 20:39:27 -0300

    Add no-backup mobile directories

--- apps/ethos-mobile/src/App.tsx
+++ apps/ethos-mobile/src/App.tsx
@@ -1,9 +1,14 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AppNavigator } from "./navigation/AppNavigator";
+import { ensureAppDirectories } from "./storage/appDirectories";
 
 const App = () => {
+  useEffect(() => {
+    void ensureAppDirectories();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AppNavigator />
